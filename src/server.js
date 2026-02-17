216314b30d765f13cd2e109580f3c9b825648883
Iryna Vysochan <ivysochan@gmail.com>
2026-02-17 21:49:16 +0200

    Fix pino-http logger configuration

--- src/server.js
+++ src/server.js
@@ -9,21 +9,8 @@ const PORT = process.env.PORT ?? 3000;
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(pino({ level: 'info' }));
+
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
