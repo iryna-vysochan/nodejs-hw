2b5968f1e4b981d546f93d4a72ca5e57bfeb6e1a
Iryna Vysochan <ivysochan@gmail.com>
2026-02-27 21:04:48 +0200

    Fix errorHandler to properly handle HttpError

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,13 +1,22 @@
 // src/middleware/errorHandler.js
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
-
+  const isHttpError = err instanceof HttpError;
   const isProd = process.env.NODE_ENV === "production";
+  const status = isHttpError ? err.status : 500;
 
-  res.status(500).json({
-    message: isProd
-      ? "Something went wrong. Please try again later."
-      : err.message,
-  });
+   const response = {
+    message:
+      isProd && !isHttpError
+        ? 'Something went wrong. Please try again later.'
+        : err.message,
+  };
+
+  if (isHttpError) {
+    response.name = err.name;
+  }
+
+  res.status(status).json(response);
 };
