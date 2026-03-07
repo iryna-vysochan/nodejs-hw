6df134a562f09bbb5a450783e6920b90111c8567
Iryna Vysochan <ivysochan@gmail.com>
2026-03-07 16:17:47 +0200

    add filtering, pagination, and validation for GET /notes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
+import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -20,6 +21,9 @@ app.use(notesRoutes);
 
 // 404 — якщо маршрут не знайдено
 app.use(notFoundHandler);
+
+// обробка помилок від celebrate (валідація)
+app.use(errors());
 
 // Error — якщо під час запиту виникла помилка
 app.use(errorHandler);
---
+++ src/validations/notesValidation.js
@@ -0,0 +1,45 @@
+import { Joi, Segments } from 'celebrate';
+import mongoose from 'mongoose';
+import { TAGS } from '../constants/tags.js';
+
+const isValidObjectId = (value, helpers) => {
+  if (!mongoose.isValidObjectId(value)) {
+    return helpers.message('Invalid ObjectId');
+  }
+  return value;
+};
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid(...TAGS),
+    search: Joi.string().allow('')
+  })
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(isValidObjectId).required()
+  })
+};
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS)
+  })
+};
+
+export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(isValidObjectId).required()
+  }),
+
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS)
+  }).min(1)
+};
